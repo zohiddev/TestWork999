615afd34e7ad11c658256bd512cc77ac7c6561aa
zohiddev <67470083+zohiddev@users.noreply.github.com>
2025-07-30 18:47:14 +0500

    add metada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import { Inter } from 'next/font/google'
 import '@/assets/scss/globals.scss'
+import { Metadata } from 'next'
 
 const inter = Inter({
   subsets: ['latin', 'cyrillic'],
   weight: ['400', '500', '600', '700'],
   display: 'swap',
 })
+
+export const metadata: Metadata = {
+  title: 'Abelohost Shop',
+  description: 'Modern e-commerce platform',
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
